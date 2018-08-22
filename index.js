088b2bdbe407643f5ba0bed58c821afa0f6c636a
jvpink200 <jvillarr20@gmail.com>
2018-08-22 03:35:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians,instruments){
   var newArry = [];
   for(var i=0; i < musicians.length; i++){
-    newArry.push(musicians[i] + "plays" + "instruments[i]");
+    newArry.push(musicians[i] + " plays " + "instruments[i]");
   }
   return newArry;
 }
